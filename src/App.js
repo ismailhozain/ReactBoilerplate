80f79383113e2d5fba452c06174338202a89205d
Ismail Hozain <ismailhozain@gmail.com>
2019-10-11 21:15:29 -0500

    added antd

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {/*ok so basically wes gonna have some bars, a big search thingy and yeah idk what else, lemme commit bigthink tonight*/}
+        <p>bigbrain</p>
         <a
           className="App-link"
           href="https://reactjs.org"
