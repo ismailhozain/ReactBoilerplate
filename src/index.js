4a4dd60b6a4107b8440fd08ff96bb6e0e0c7964a
ismailhozain <36ismailhozain@celinaisd.com>
2019-10-21 17:55:21 -0500

    partially set up firebase.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,8 @@ import App from './App';
 import "antd/dist/antd.css";
 import * as serviceWorker from './serviceWorker';
 import { Layout, Menu, Breadcrumb } from 'antd';
-// Your web app's Firebase configuration
 
 ReactDOM.render(<App />, document.getElementById('root'));
-//LOOK KIDS. YOU PROBABLY SHOULDNT TOUCH THIS FILE MUCH.
-
-
-
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
