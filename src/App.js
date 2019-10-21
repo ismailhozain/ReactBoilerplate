fcdbff4e4159b18c6051495ea0d3f651ede5f5d9
Ismail Hozain <ismailhozain@gmail.com>
2019-10-20 21:54:42 -0500

    made basic frontend, and added-react router

--- src/App.js
+++ src/App.js
@@ -1,100 +1,57 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import { Form, Input, Menu, Dropdown, Icon } from 'antd';
 const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
+const searchBar = {
 
+
+};
 function App() {
   return (
-    <div className="App">
-      <Layout>
-      <Header className="header">
-        <div className="logo" />
-        <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-        >
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
-      <Layout>
-        <Sider width={200} style={{ background: '#fff' }}>
-          <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%', borderRight: 0 }}
-          >
-            <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                <Icon type="user" />
-                subnav 1
-              </span>
-                }
-            >
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu
-                key="sub2"
-                title={
-                  <span>
-                <Icon type="laptop" />
-                subnav 2
-              </span>
-                }
-            >
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu
-                key="sub3"
-                title={
-                  <span>
-                <Icon type="notification" />
-                subnav 3
-              </span>
-                }
-            >
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-              style={{
-                background: '#fff',
-                padding: 24,
-                margin: 0,
-                minHeight: 280,
-              }}
-          >
-            Content
-          </Content>
-        </Layout>
-      </Layout>
-    </Layout>
-    </div>
-  );
-}
+      <div className="App">
+          <Router>
+              <Menu mode="horizontal">
+                  <Menu.Item>
+                      <Link to={"/"}><Icon type="home" /> Home</Link>
+                  </Menu.Item>
+                  <Menu.Item>
+                      <Link to={"/about"}><Icon type="info-circle" /> About</Link>
+                  </Menu.Item>
+                  <Menu.Item>
+                      <Link to={"/login"}><Icon type="login" /> Login</Link>
+                  </Menu.Item>
+              </Menu>
+              <Form>
+                 <Input className="searchBar"
+                        prefix={<Icon type="search" />}
+                 />
+              </Form>
+              <div>
+                  <h1>
+                      What would you like to do today?
+                  </h1>
+                  <Menu>
+                      <Menu.Item>
+                          Check On Life.
+                      </Menu.Item>
+                      <Menu.Item>
+                          Learn Something New!
+                      </Menu.Item>
+                      <Menu.Item>
+                          Study Some Content.
+                      </Menu.Item>
+                  </Menu>
+              </div>
+          </Router>
 
+      </div>
+
+  )}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//react stuff above, add new pages beneath.
 import './index.css';
 import App from './App';
 import "antd/dist/antd.css";
