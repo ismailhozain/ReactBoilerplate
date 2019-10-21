e2455fbb10a52114e4638087cf39a51ddc8b3840
ismailhozain <36ismailhozain@celinaisd.com>
2019-10-21 17:56:32 -0500

    partially set up firebase.y

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ import { Layout, Menu, Breadcrumb } from 'antd';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note this comes with some pitfalls.wertyui
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
