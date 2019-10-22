2781b9279fa981e2a074905b22bd88d5fcc5f30e
ismailhozain <36ismailhozain@celinaisd.com>
2019-10-22 09:55:32 -0500

    redid project structure cuz im dum

---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/pw-forget';
+export const PROFILE = '/profile';
+export const ADMIN_DETAILS = '/admin/:id';
+//ADD A LINE FOR EACH NEW PAGE YOU MAKE SO IT KEEPS CONSISTENCY.
