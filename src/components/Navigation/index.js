b601c88f4ad2f637c90103e0bcfa3da944da9076
ismailhozain <36ismailhozain@celinaisd.com>
2019-11-01 16:18:05 -0500

    reinstalled antd. experimented with styling <Link> entries.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+    NavLink
+} from "react-router-dom";
+
+import { Menu, Icon} from 'antd';
+import { Button } from 'antd';
 const Navigation = () => (
     <div>
-        <ul>
-            <li>
-                <button>
-                    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
 
-                </button>
-            </li>
-            <li>
+        <Menu mode="horizontal">
+            <Menu.Item componenet={Link} to={ROUTES.SIGN_IN} key="signin">Sign In</Menu.Item>
+            <Menu.Item key="landing">
                 <Link to={ROUTES.LANDING}>Landing</Link>
-            </li>
-            <li>
+            </Menu.Item>
+            <Menu.Item key="home">
                 <Link to={ROUTES.HOME}>Home</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.ACCOUNT}>Account</Link>
-            </li>
-            <li>
+            </Menu.Item>
+            <Menu.Item key="admin">
                 <Link to={ROUTES.ADMIN}>Admin</Link>
-            </li>
-        </ul>
+            </Menu.Item>
+        </Menu>
     </div>
 );
 export default Navigation;
