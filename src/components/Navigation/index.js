558c64d87ed8dc9bd3b90aa1190142317995e6d8
Ismail Hozain <ismailhozain@gmail.com>
2019-11-02 13:04:46 -0500

    finished navbar. figured out antd stuff.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
+import {Menu} from "antd";
+import 'antd/dist/antd.css';
+
 const Navigation = () => (
     <div>
-        <ul>
-            <li>
-                <button>
+        <Menu mode={"horizontal"}>
+                <Menu.Item>
                     <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-
-                </button>
-            </li>
-            <li>
-                <Link to={ROUTES.LANDING}>Landing</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.HOME}>Home</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.ACCOUNT}>Account</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.ADMIN}>Admin</Link>
-            </li>
-        </ul>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to={ROUTES.LANDING}>Landing</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to={ROUTES.HOME}>Home</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to={ROUTES.ACCOUNT}>Account</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to={ROUTES.ADMIN}>Admin</Link>
+                </Menu.Item>
+        </Menu>
     </div>
 );
 export default Navigation;
