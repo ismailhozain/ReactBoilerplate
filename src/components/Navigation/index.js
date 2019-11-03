63a7cd266154803d28882cc5eb561d3e52684df1
Ismail Hozain <ismailhozain@gmail.com>
2019-11-03 12:29:24 -0600

    added favicon implementations and logo and like idk

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import {Menu, Icon} from "antd";
 import 'antd/dist/antd.css';
-
+import Logo from './logo.svg';
+import './navigation.css';
 const Navigation = () => (
     <div>
         <Menu mode={"horizontal"}>
                 <Menu.Item>
-                    <Link to={ROUTES.SIGN_IN}><Icon type="login" />Sign In</Link>
+                    <Link to={ROUTES.LANDING}><img src={Logo}/></Link>
                 </Menu.Item>
                 <Menu.Item>
-                    <Link to={ROUTES.LANDING}><Icon type="thunderbolt" />Landing</Link>
+                    <Link to={ROUTES.SIGN_IN}><Icon type="login" />Sign In</Link>
                 </Menu.Item>
                 <Menu.Item>
                     <Link to={ROUTES.HOME}><Icon type="dashboard" />Home</Link>
