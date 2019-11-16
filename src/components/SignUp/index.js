54fa56ce6176e33b78ac7a2de9a93cce4f2f88d2
ismailhozain <36ismailhozain@celinaisd.com>
2019-11-16 15:34:33 -0600

    tiny changes updated signup form, simplified it.

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -20,6 +20,7 @@ class NormalLoginForm extends React.Component {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
+                alert("Your password is the same as your canvas password");
                 console.log('Received values of form: ', values);
             }
         });
@@ -29,7 +30,9 @@ class NormalLoginForm extends React.Component {
         const { getFieldDecorator } = this.props.form;
         return (
             <div className={"center wholeDiv"}>
+
                 <Form onSubmit={this.handleSubmit} className="login-form">
+                    <h2 className={"center title"}>Studyboard</h2>
                     <Form.Item>
                         {getFieldDecorator('firstName', {
                             rules: [{ required: true, message: 'Please input your first name!' }],
@@ -61,66 +64,12 @@ class NormalLoginForm extends React.Component {
                         )}
                     </Form.Item>
                     <Form.Item>
-                        {getFieldDecorator('username', {
-                            rules: [{ required: true, message: 'Please input your username!' }],
+                        {getFieldDecorator('schoolId', {
+                            rules: [{ required: true, message: 'Please input your school ID!' }],
                         })(
                             <Input
                                 prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('passwordOne', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('passwordTwo', {
-                            rules: [{ required: true, message: 'Please input your password again!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />,
-                        )}
-                    </Form.Item>
-
-                    <Form.Item>
-                        {getFieldDecorator('hacUsername', {
-                            rules: [{ required: true, message: 'Please input your HAC username!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="HAC Username"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('hacPassword', {
-                            rules: [{ required: true, message: 'Please input your HAC password!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="HAC Password"
-                                type={"password"}
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('canvasUsername', {
-                            rules: [{ required: true, message: 'Please input your Canvas username!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Canvas Username"
+                                placeholder="School ID"
                             />,
                         )}
                     </Form.Item>
@@ -142,9 +91,8 @@ class NormalLoginForm extends React.Component {
                         })(<Checkbox>Remember me</Checkbox>)}
                         <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password</Link>
                         <Button type="primary" htmlType="submit" className="login-form-button">
-                            Sign Up!
+                            <Link to={ROUTES.HOME}>Sign Up!</Link>
                         </Button>
-                        Or <Link to={ROUTES.SIGN_IN}>Log In!</Link>
                     </Form.Item>
                 </Form>
             </div>
