470b5f5a5235a90bd291e71e347b87608ba28d8a
Ismail Hozain <ismailhozain@gmail.com>
2019-11-26 21:47:23 -0600

    moving to webstorm. lets hope this fragile af team can handle it.

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { compose } from 'recompose';
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 import {Icon} from "antd";
@@ -22,17 +25,15 @@ class SignInFormBase extends Component {
         this.state = { ...INITIAL_STATE };
     }
     onSubmit = event => {
-        const { email, password } = this.state;
-        this.props.firebase
-            .doSignInWithEmailAndPassword(email, password)
-            .then(() => {
-                this.setState({ ...INITIAL_STATE });
-                this.props.history.push(ROUTES.HOME);
-            })
-            .catch(error => {
-                this.setState({ error });
-            });
-        event.preventDefault();
+        const {email, password} = this.state;
+        {/*
+        ONSUBMIT GETS CALLED AND WORKS FINE.
+        I CANT SAY THE SAME ABOUT THE REST OF IT.
+        OK I THINK I KNOW WHAT IS WRONG. WHEN I SEND AN EXPLICIT VALUE TO IT IT REFUSES TO SIGN IN.
+        IM THINKING THAT MEANS THAT THE WHOLE FUNCTION IS WRONG. RESTART TIME but yeah this needs to change. TODO REWRITE THIS FUNCTION.
+        */}
+
+            event.preventDefault();
     };
     onChange = event => {
         this.setState({ [event.target.name]: event.target.value });
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -6,6 +6,5 @@ const SignOutButton = ({ firebase }) => (
     </button>
 
 );
-{/*PRETTY DAMN SURE THIS WORKS ALSO THINK THAT MY SIGN IN FUNCTION DOESNT SO THATS FUN GOTTA GO FIX THAT. made good progress
- might rewrite the front end tomorrow morning. I would like to have the whole thing done by the end of thanksgiving break*/}
+
 export default withFirebase(SignOutButton);
