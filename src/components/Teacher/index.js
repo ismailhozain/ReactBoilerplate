dee806733c8a6782ce8cfa078eb05e016083b057
ismailhozain <ismailhozain@gmail.com>
2020-02-19 12:33:42 -0600

    reinitialized firebase. I think i fixed my security issues in the db.

--- src/components/Teacher/index.js
+++ src/components/Teacher/index.js
@@ -8,7 +8,6 @@ class TeacherPage extends Component {
         super(props);
 
         this.state = {
-            loading: false,
             users: [],
         };
     }
@@ -19,10 +18,9 @@ class TeacherPage extends Component {
             const usersList = Object.keys(usersObject).map(key => ({
                 ...usersObject[key],
                 uid: key,
-            }))
+            }));
             this.setState({
                 users: usersList,
-                loading: false,
             });
         });
     }
@@ -31,12 +29,9 @@ class TeacherPage extends Component {
     }
 
     render() {
-        const {users, loading } = this.state;
+        const {users} = this.state;
         return (
             <div>
-                {loading && <div>
-                    Loading...
-                </div>}
                 <UserList users={users}/>
             </div>
         )
